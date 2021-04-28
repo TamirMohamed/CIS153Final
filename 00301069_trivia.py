3043f682b5f859ce0ed64bbe719c15e23762a216
TamirMohamed <82790614+TamirMohamed@users.noreply.github.com>
2021-04-28 06:11:55 -0400

    Add files via upload

--- 00301069_trivia.py
+++ 00301069_trivia.py
@@ -2,19 +2,14 @@
 
 from tkinter import *
 import random
-import time
+
 
 class Application(Frame):
 
-    def getRandomNum(self): #Gets random number between 1 and 40
-        num = random.randrange(1, 40)
-        return num
-
-    def __init__(self, master): #initialize frame
+    def __init__(self, master): #Initialize frame
         Frame.__init__(self, master) 
         self.grid()
         self.makeElements()
-        self.number = random.randint(1, 101)
         self.columnconfigure(1, weight=1)
         self.columnconfigure(2, weight=1)
         self.columnconfigure(3, weight=1)
@@ -22,9 +17,9 @@ class Application(Frame):
         self.rowconfigure(3, weight=1)
 
     def makeElements(self):
-        
+        #Place elements (rough)
         Button(self, text = "Play", command = self.playGame).grid(row = 0, column = 0, sticky = W)
-        Button(self, text = "Help", command = self.playGame).grid(row = 0, column = 1, sticky = W)
+        Button(self, text = "Help", command = self.playGame).grid(row = 0, column = 1, sticky = W) #Help button function to be implemented
         Button(self, text = "Statistics", command = self.getStats).grid(row = 0, column = 2, sticky = W)
 
         Label(self, text = "Answer:").grid(row = 2, column = 0, sticky = W)
@@ -38,36 +33,41 @@ class Application(Frame):
         Label(self, text = "Press submit to check your answer:").grid(row = 3, column = 0, sticky = W)
         Button(self, text = "Submit", command = self.checkAnswer).grid(row = 3, column = 1, sticky = W)
 
-        instructions = 'Enter a username and press "stats" to get statistics for that user. Press "help" for instructions on how to play the game. Otherwise press play to get quizzed!'
+        instructions = 'Enter a username and press "Statistics" to get statistics for that user. \nPress stats with no username for general user information. \nPress "help" for instructions on how to play the game. \nOtherwise enter a username and press play to get quizzed!'
 
-        self.text = Text(self, width = 70, height = 10, wrap = WORD)
-        self.text.grid(row = 4, column = 0, columnspan = 3)
+        self.textBox = Text(self, width = 70, height = 10, wrap = WORD)
+        self.textBox.grid(row = 4, column = 0, columnspan = 3)
         
-        self.text.insert(0.0, instructions)
+        self.textBox.insert(0.0, instructions)
+        
+    def getRandomNum(self): #Gets random number for question index
+        num = random.randrange(1, 40)
+        return num
 
     def playGame(self):
-        print_text=''
+        textToPrint=''
         questions = open('trivia.txt', encoding="utf8")
         rand = self.getRandomNum()
-        if self.username.get() == '':
-            self.text.delete(0.0, END)
-            self.text.insert(0.0, "You need to insert a username before playing!")
+        if self.username.get() == '': #Username field must be filled to play
+            self.textBox.delete(0.0, END)
+            self.textBox.insert(0.0, "You need to insert a username before playing!")
         else:
             for line in questions:
                 line = line.rstrip()
-                if line.startswith(str(rand) + ")"):
+                if line.startswith(str(rand) + ")"): #Find question at random index in file(to be seperated into function)
                     string = line
                     self.questionNumber = rand
+                    #Parse line
                     numberPlace = string.find(')')
                     firstOption = string.find('A)')
                     answerPlace = string.find('Answer: ')
                     self.question = (string[numberPlace+2:firstOption])
                     options = (string[firstOption:answerPlace])
                     self.ans = (string[answerPlace+8:])
-                    print_text += self.question
-                    print_text += "\n\n" + options
-            self.text.delete(0.0, END)
-            self.text.insert(0.0, print_text)
+                    textToPrint += self.question
+                    textToPrint += "\n\n" + options
+            self.textBox.delete(0.0, END)
+            self.textBox.insert(0.0, textToPrint)
         
     def checkAnswer(self):
         logFile = open('log.txt', 'a')
@@ -76,22 +76,22 @@ class Application(Frame):
         username = self.username.get()
         log = ''
         if self.username.get() == '':
-            self.text.delete(0.0, END)
-            self.text.insert(0.0, "Please make sure you have entered a username.")
+            self.textBox.delete(0.0, END)
+            self.textBox.insert(0.0, "Please make sure you have entered a username.")
         else:
             if guess.upper() != self.ans:
-                print_text = "You guessed incorrectly. Press 'Play' to get a new question!"
+                textToPrint = "You guessed incorrectly. Press 'Play' to get a new question!"
                 log += "USERNAME: " + username + " QUESTION: " + str(self.questionNumber) + " RESULT: " + isCorrect + "\n"
-                self.text.delete(0.0, END)
-                self.text.insert(0.0, print_text)
+                self.textBox.delete(0.0, END)
+                self.textBox.insert(0.0, textToPrint)
                 logFile.write(log)
                 self.answerEntry.delete(0, END)
             else:
                 isCorrect = "Correct"
-                print_text = "That's the right choice! Well done! Press 'Play' for a new question!"
+                textToPrint = "That's the right choice! Well done! Press 'Play' for a new question!"
                 log += "USERNAME: " + username + " QUESTION: " + str(self.questionNumber) + " RESULT: " + isCorrect + "\n"
-                self.text.delete(0.0, END)
-                self.text.insert(0.0, print_text)
+                self.textBox.delete(0.0, END)
+                self.textBox.insert(0.0, textToPrint)
                 logFile.write(log)
                 self.answerEntry.delete(0, END)
                 
@@ -100,25 +100,25 @@ class Application(Frame):
         max = 0
         maxAddress = ''
         logFile = open('log.txt')
-        if self.username.get() == '': #If username field is blank
+        if self.username.get() == '': #If username field isn't filled in, get general stats
             for line in logFile:
                 line = line.rstrip()
                 string = line
                 usernamePlace = string.find('USERNAME: ')
                 questionPlace = string.find('QUESTION: ')
                 username = (string[usernamePlace+10:questionPlace])
-                if username not in users:
-                    users[username] = 1 #Count first instance
+                if username not in users: #Fill users dict
+                    users[username] = 1 
                 else:
-                    users[username] += 1 #Count further
+                    users[username] += 1 
                     
-            for username in users:  #For all addresses
-                if users[username] > max: #If new max  
-                    max = users[username] #Store max
-                    maxAddress = username   #Store address
-            self.text.delete(0.0, END)
-            self.text.insert(0.0, "The user that made the most guesses is " + maxAddress + "and they made " + str(max) + " guesses.")
-        else:
+            for username in users:  #For all usernames
+                if users[username] > max: #Find max
+                    max = users[username] 
+                    maxAddress = username  
+            self.textBox.delete(0.0, END)
+            self.textBox.insert(0.0, "The user that made the most guesses is " + maxAddress + "and they made " + str(max) + " guesses.")
+        else: #Otherwise show specific user stats
             numCorrect = 0
             numWrong = 0
             username = self.username.get()
@@ -136,8 +136,8 @@ class Application(Frame):
                         numCorrect+=1
                     else:
                         numWrong+=1
-            self.text.delete(0.0, END)            
-            self.text.insert(0.0, "User '" + username + "' made " + str(numCorrect + numWrong) + " total guesses. " + str(numCorrect) + " were correct and " + str(numWrong) + " were incorrect.")
+            self.textBox.delete(0.0, END)            
+            self.textBox.insert(0.0, "User '" + username + "' made " + str(numCorrect + numWrong) + " total guesses. " + str(numCorrect) + " were correct and " + str(numWrong) + " were incorrect.")
 
         
 root = Tk()
